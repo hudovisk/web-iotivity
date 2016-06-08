9dc7c318d2346352133dcaabbba02f72872c2499
Hudo Assenco <hudo.assenco@gmail.com>
2016-06-07 19:48:27 -0700

    Update server

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ io.on('connection', function(socket){
 
     for(var i=0; i < devices.lenght; i++) {
       if(devices[i].id === resp.id) {
-        devices[i].attrs = resp.attrs;
+        devices[i].attrs = resp.attrs.slice(0);
         break;
       }
     }
